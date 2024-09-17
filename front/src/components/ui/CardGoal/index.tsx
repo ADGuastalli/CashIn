ac85c41f889c78ec832790fb70afdc2b7957b4f4
maxi-pereyra <maxiabelpereyra@hotmail.com.ar>
2024-09-17 16:17:41 -0300

    perfil

--- front/src/components/ui/CardGoal/index.tsx
+++ front/src/components/ui/CardGoal/index.tsx
@@ -10,7 +10,7 @@ function CardGoal() {
         </div>
         <div className='flex-1 m-2 '>
             <div className="flex items-start justify-around m-2">
-            <h4 className="font-medium text-sm mr-auto text-gray-700 flex items-center mr-2">
+            <h4 className="font-medium text-sm mr-auto text-gray-700 flex items-center">
                 nombre meta
             </h4>
             <span className="px-2 py-1 rounded-lg bg-red-50 text-red-500 text-xs">
