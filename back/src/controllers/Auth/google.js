fc5e7af72b1c4640f0e6dd3c9d18e7790900cf90
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-09-24 14:22:27 -0300

    Paypal

--- back/src/controllers/Auth/google.js
+++ back/src/controllers/Auth/google.js
@@ -1,21 +1,29 @@
 // controllers/auth/googleAuthController.js
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const { User } = require('../../models/User'); // Ajustar según la ubicación de tu modelo de usuario
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models/User"); // Ajustar según la ubicación de tu modelo de usuario
 
 // Estrategia de Google
 const googleAuth = (req, res) => {
-  passport.authenticate('google', { failureRedirect: '/' }, async (err, user) => {
-    if (err || !user) {
-      return res.status(401).json({ error: 'Error al autenticar con Google' });
+  passport.authenticate(
+    "google",
+    { failureRedirect: "/" },
+    async (err, user) => {
+      if (err || !user) {
+        return res
+          .status(401)
+          .json({ error: "Error al autenticar con Google" });
+      }
+
+      const jwtSecret = process.env.JWT_SECRET;
+      const token = jwt.sign({ userId: user.id }, jwtSecret, {
+        expiresIn: "24h",
+      });
+
+      // Redirigir al frontend con el token
+      res.redirect(`http://localhost:3000/profile?token=${token}`);
     }
-
-    const jwtSecret = process.env.JWT_SECRET;
-    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '24h' });
-
-    // Redirigir al frontend con el token
-    res.redirect(`http://localhost:3000/profile?token=${token}`);
-  })(req, res);
+  )(req, res);
 };
 
 module.exports = {
